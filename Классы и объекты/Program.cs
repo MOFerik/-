332f9bcd073214670897437d28dcb6c9971b02ed
MOFerik <73346667+MOFerik@users.noreply.github.com>
2020-11-08 18:05:03 +0500

    Завершен перввый класс наследник
    Реализованы все конструкторы класса наследника, а так же создана функция для него.

--- Классы и объекты/Program.cs
+++ Классы и объекты/Program.cs
@@ -68,16 +68,59 @@ namespace Классы_и_объекты
     class EarthLike : Planet
     {
         private double sTemp;
-        private float water;
+        private bool water;
+
+        public void LifeProb()
+        {
+            if (this.radius * this.density > 0.5 && this.radius * this.density < 1.5 && this.sTemp < 50 && this.sTemp > -30 && water)
+                Console.WriteLine($"{this.name} is likely to be habitable\n");
+            else
+                Console.WriteLine($"{this.name} is probably not habitable\n");
+        }
 
         public EarthLike()
         {
+            sTemp = 1.2;
+            water = false;
             Console.WriteLine($"It is an Earth like planet. It's surface temperature is the same as Earth's and it doesn't have any water.\n");
         }
 
         public EarthLike(string name) : base(name)
         {
+            sTemp = 1.2;
+            water = false;
             Console.WriteLine($"It is an Earth like planet. It's surface temperature is the same as Earth's and it doesn't have any water.\n");
+        }
+
+        public EarthLike(string name, double rad) : base(name, rad)
+        {
+            sTemp = 1.2;
+            water = false;
+            Console.WriteLine($"It is an Earth like planet. It's surface temperature is the same as Earth's and it doesn't have any water.\n");
+        }
+
+        public EarthLike(string name, double rad, double dens) : base(name, rad, dens)
+        {
+            sTemp = 1.2;
+            water = false;
+            Console.WriteLine($"It is an Earth like planet. It's surface temperature is the same as Earth's and it doesn't have any water.\n");
+        }
+
+        public EarthLike(string name, double rad, double dens, double temp) : base(name, rad, dens)
+        {
+            sTemp = temp;
+            water = false;
+            Console.WriteLine($"It is an Earth like planet. It's surface temperature is {this.sTemp} Celsius and it doesn't have any water.\n");
+        }
+
+        public EarthLike(string name, double rad, double dens, double temp, bool wat) : base(name, rad, dens)
+        {
+            sTemp = temp;
+            water = wat;
+            if (water)
+                Console.WriteLine($"It is an Earth like planet. It's surface temperature is {this.sTemp} Celsius and it has liquid water.\n");
+            else
+                Console.WriteLine($"It is an Earth like planet. It's surface temperature is {this.sTemp} Celsius and it doesn't have any water.\n");
         }
     };
 
@@ -85,13 +128,15 @@ namespace Классы_и_объекты
     {
         static void Main(string[] args)
         {
-            EarthLike pl0 = new EarthLike("Earth");
-            Planet pl1 = new Planet("X01");
-            Planet pl2 = new Planet("Jupiter", 70);
-            Planet pl3 = new Planet("Mars", 0.5, 0.7);
-            Planet pl4 = new Planet(pl1);
+            EarthLike pl0 = new EarthLike("Hyperion", 2, 0.8, 13.5, true);
+            pl0.CalcGrav();
+            pl0.LifeProb();
+            //Planet pl1 = new Planet("X01");
+            //Planet pl2 = new Planet("Jupiter", 70);
+            //Planet pl3 = new Planet("Mars", 0.5, 0.7);
+            //Planet pl4 = new Planet(pl1);
 
-            pl3.CalcGrav();
+            //pl3.CalcGrav();
         }
     }
 }
